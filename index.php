0474538d03adaef8c28039b58ea991ba50a706c5
nordialog <helge.andersen@nordialog.no>
2016-10-25 22:53:08 +0000

    yes!

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	</head>
 	<body>
-    	<h1>Dritbra!</h1>
+    	<h1>Dritbra, der funker det fjell!</h1>
     <p>Trur eg, heilt sikkert..., kanskje, eller?</p>
     <p>Dette funker faktisk, tenk det!!</p>
 	</body>
