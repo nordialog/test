3e76e78b677c528ace861a747c7a1510bed3e412
Helge Andersen <helge.andersen@nordialog.no>
2016-10-26 16:43:06 +0200

    Coda 2

--- index.php
+++ index.php
@@ -12,5 +12,13 @@ ini_set('display_errors', 1);
     <p>Trur eg, heilt sikkert..., kanskje, eller?</p>
     <p>Dette funker faktisk, tenk det!!</p>
         <p><img src="storage/plakat01.jpg" style="width:450px;height:auto;"></p>
+        <?php
+          $myfile = fopen("storage/newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);  
+        ?>
 	</body>
 </html>
