85d115b00469b2edf4c39b0a9b00914a2416b794
Helge Andersen <helge.andersen@nordialog.no>
2016-10-26 08:53:58 +0200

    Coda test

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
     	<h1>Dritbra, der funker det fjell!</h1>
     <p>Trur eg, heilt sikkert..., kanskje, eller?</p>
     <p>Dette funker faktisk, tenk det!!</p>
+        <?php
+$myfile = fopen("text/newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
 	</body>
 </html>
